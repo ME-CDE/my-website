beabeb686106208caa9dce87d4263bd83ac21bc1
ME-CDE <godswillanwuli@gmail.com>
2023-01-10 18:28:14 +0100

    cv

--- src/components/Me.vue
+++ src/components/Me.vue
@@ -2,7 +2,10 @@
   <v-container class="max-w-xl">
     <div class="w-100 section-1">
       <div class="imgdiv">
-        <img class="img bg-buttonBg rounded-circle elevation-12" src="/assets/myprofiepic.jpg"/>
+        <img
+          class="img bg-buttonBg rounded-circle elevation-12"
+          src="/assets/myprofiepic.jpg"
+        />
         <div class="name-div mt-5 pl-3">
           <h1 class="text-h4 font-weight-bold">Godswill Anwuli</h1>
           <p class="font-weight-medium">Javascript | web developer | Nigeria</p>
@@ -17,14 +20,17 @@
           tech. I use javascript tools like react, vue, firebase e.t.c and some
           other tools to bring ideas to reality. Let's connect and collabrate.
         </p>
-        <v-btn
-          size="large"
-          class="text-buttonText bg-buttonBg mt-5"
-          append-icon="mdi-download"
-        >
-          <p class="d-md-none"><span class="my">my</span> cv</p>
-          <p class="d-none d-md-inline-block">my resume</p>
-        </v-btn>
+        
+          <v-btn
+            size="large"
+            class="text-buttonText bg-buttonBg mt-5"
+            append-icon="mdi-download"
+            @click="DownloadFile('GODSWILL-ANWULI-RESUME.PDF')"
+          >
+            <p class="d-md-none"><span class="my">my</span> cv</p>
+            <p class="d-none d-md-inline-block">my resume</p>
+          </v-btn>
+
         <v-btn
           size="large"
           class="text-buttonText bg-buttonBg mt-5 ml-5 get-in-touch"
@@ -52,8 +58,36 @@ export default {
     };
   },
   methods: {
-    anotherPage(value) {
-      window.location.replace(value);
+    DownloadFile(fileName) {
+      //Set the File URL.
+      const url = "assets/" + fileName;
+
+      //Create XMLHTTP Request.
+      const req = new XMLHttpRequest();
+      req.open("GET", url, true);
+      req.responseType = "blob";
+      req.onload = function () {
+        //Convert the Byte Data to BLOB object.
+        const blob = new Blob([req.response], {
+          type: "application/octetstream",
+        });
+
+        //Check the Browser type and download the File.
+        const isIE = false || !!document.documentMode;
+        if (isIE) {
+          window.navigator.msSaveBlob(blob, fileName);
+        } else {
+          const url = window.URL || window.webkitURL;
+          const link = url.createObjectURL(blob);
+          const a = document.createElement("a");
+          a.setAttribute("download", fileName);
+          a.setAttribute("href", link);
+          document.body.appendChild(a);
+          a.click();
+          document.body.removeChild(a);
+        }
+      };
+      req.send();
     },
   },
 };
